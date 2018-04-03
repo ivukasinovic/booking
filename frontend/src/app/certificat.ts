32077e70cd9ef5c42acc3795568db3cd3322f74b
Smekac <stojkic95@hotmail.com>
2018-04-03 16:48:29 +0200

    Izmena samo da se proveri checkButton za Issuer-a

---
+++ frontend/src/app/certificat.ts
@@ -0,0 +1,14 @@
+
+
+export class Certificat {
+
+  issuer: string;
+  CN: string;
+  SN: string;
+  GN: string;
+  ON: string;
+  LN: string;
+  C: string;
+  E: string;
+  forCA: Boolean;
+}
--- frontend/src/app/certificat/certificat.component.ts
+++ frontend/src/app/certificat/certificat.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Input} from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-certificat',
@@ -15,10 +16,10 @@ export class CertificatComponent implements OnInit {
   niz = [];
 
   // Od dugmeta kada  potvrdi da pravi sertifikat ...
-  addNiz(CN: string , SN: string , GN: string, ON: string, LN: string, Country: string, Email: string) {
+  addNiz(CN: string , SN: string , GN: string, ON: string, LN: string, Country: string, Email: string, CA: boolean) {
     if (CN !== '' && SN !== '' && GN !== '' && ON !== '' && LN !== '' && Country !== '' && Email !== '') {
       this.niz.push({
-        CN: CN, SN: SN, GN: GN, ON: ON, LN: LN, C: Country, E: Email
+        CN: CN, SN: SN, GN: GN, ON: ON, LN: LN, C: Country, E: Email, CA: CA
       });
     }
 
