06bd25ea02dec6c94e988ef46a7b610788241222
Smekac <stojkic95@hotmail.com>
2018-04-03 10:08:32 +0200

    Frontend za Kreiranje KeyStor-a i Sertifikata

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,16 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatCheckboxModule} from '@angular/material';
+import { MatCheckboxModule, MatStepperModule, MatSelectModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import { AppRoutingModule } from './/app-routing.module';
+import { CertificateComponent } from './certificate/certificate.component';
+import { KeyStoreComponent } from './key-store/key-store.component';
+import { SelectKeyStoreComponent } from './select-key-store/select-key-store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CertificateComponent,
+    KeyStoreComponent,
+    SelectKeyStoreComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatStepperModule,
+    MatSelectModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
