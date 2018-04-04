7bbd6c1ae1b867f120f16e63c0f580fbbecd4952
Ivan Vukasinovic <32752225+ivukasinovic@users.noreply.github.com>
2018-04-04 18:42:40 +0200

    Ispravan objekat sertifikat poslan (#4)

--- frontend/src/app/certificates/certificat/certificat.component.ts
+++ frontend/src/app/certificates/certificat/certificat.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {CertificatesService} from '../certificates.service';
+import {Router} from '@angular/router';
+import {Certificate} from '../model';
 
 @Component({
   selector: 'app-certificat',
@@ -10,20 +13,40 @@ export class CertificatComponent implements OnInit {
   niz = [];
   samoCekirani = [];        // Samo one kod kojih je cekiran cek-Box
   saveUsername: boolean;
+  private certificate: Certificate;
 
-  constructor() {
+  constructor(private cerService: CertificatesService, private router: Router) {
+    this.certificate = new Certificate();
+
+    this.certificate.issuerName = '';
+
   }
 
   ngOnInit() {
+
+    this.saveUsername = false;
   }
 
 
-  // Od dugmeta kada  potvrdi da pravi sertifikat ...
-  addNiz(CN: string, SN: string, GN: string, ON: string, LN: string, Country: string, Email: string, CA: boolean) {
+  getCertificate(SELEC: string , CN: string, SN: string, GN: string, ON: string, LN: string, Country: string, Email: string, CA: boolean) {
     if (CN !== '' && SN !== '' && GN !== '' && ON !== '' && LN !== '' && Country !== '' && Email !== '') {
+
       this.niz.push({
-        CN: CN, SN: SN, GN: GN, ON: ON, LN: LN, C: Country, E: Email, CA: CA
+        SELEC: SELEC, CN: CN, SN: SN, GN: GN, ON: ON, LN: LN, C: Country, E: Email, CA: this.saveUsername
       });
+
+      this.certificate.issuerName = SELEC;
+      this.certificate.commonName = CN;
+      this.certificate.surname = SN;
+      this.certificate.givenName = GN;
+      this.certificate.orgName = ON;
+      this.certificate.uid = LN;
+      this.certificate.country = Country;
+      this.certificate.email = Email;
+      this.certificate.isCa = this.saveUsername;
+
+      this.cerService.getCertificat(this.certificate).subscribe((result: Certificate) => {
+        this.certificate = result; });
 
       console.log(CA + '  _______ ' + this.saveUsername);
 
@@ -31,8 +54,12 @@ export class CertificatComponent implements OnInit {
         this.samoCekirani.push({CN: CN, SN: SN, GN: GN, ON: ON, LN: LN, C: Country, E: Email, CA: CA});
       }
 
+      console.log(this.certificate);
+
+      this.cerService.getCertificates();
     }
 
   } // kraj metode
+
 
 }
