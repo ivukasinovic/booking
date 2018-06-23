f8cd85272c361b200f575fb455176d46b04df2e3
ivukasinovic <ivanvukasino@gmail.com>
2018-06-23 12:50:47 +0200

    added getImages ws

--- agent/agent-server/src/main/java/com/project/ws/LodgingRes.java
+++ agent/agent-server/src/main/java/com/project/ws/LodgingRes.java
@@ -1,10 +1,14 @@
 
 package com.project.ws;
 
-import javax.xml.bind.annotation.*;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
 
 
 /**
