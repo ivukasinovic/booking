154084c1d0600699f03d6267349e2f98cf5259ed
ivukasinovic <ivanvukasino@gmail.com>
2018-06-30 00:11:37 +0200

    deleting garbage

--- backend/src/main/java/XMLandSecurity/backend1/TestData.java
+++ backend/src/main/java/XMLandSecurity/backend1/TestData.java
@@ -2,7 +2,6 @@ package XMLandSecurity.backend1;
 
 import XMLandSecurity.backend1.service.KeyStoreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -16,21 +15,22 @@ public class TestData {
     @Autowired
     private KeyStoreService keyStoreService;
 
-    public TestData(){}
+    public TestData() {
+    }
 
     @PostConstruct
     private void init() {
 //        System.out.println("Kreiranje keystorova");
 //        keyStoreService.createKeyStores();
 
-        //generating hash for passwords
-        BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
-//        System.out.println(bc.encode("Dejan 1010"));
-//        System.out.println(bc.encode("Stefan 1010"));
-//        System.out.println(bc.encode("Jovana 1010"));
-//        System.out.println(bc.encode("Ivan 1010"));
-//        System.out.println(bc.encode("Admin 1010"));
-//        System.out.println(bc.encode("Agent 1010"));
+//        //generating hash for passwords
+//        BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
+////        System.out.println(bc.encode("Dejan 1010"));
+////        System.out.println(bc.encode("Stefan 1010"));
+////        System.out.println(bc.encode("Jovana 1010"));
+////        System.out.println(bc.encode("Ivan 1010"));
+////        System.out.println(bc.encode("Admin 1010"));
+////        System.out.println(bc.encode("Agent 1010"));
 
     }
 
